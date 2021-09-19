890bb3bdb204977ae4e657dc813c9f552051f868
rov9 <duttasaurabh92@gmail.com>
2021-09-20 01:05:52 +0530

    html,css,js update from my repl repo

---
+++ quiz.js
@@ -0,0 +1,31 @@
+const quizform = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector(".btn-submit");
+const outputMessage = document.querySelector(".output");
+
+const correctAnswers = [
+  "obtuse",
+  "scalene triangle",
+  "Isosceles triangle",
+  "12, 16, 20",
+  "Equilateral triangle",
+  "100°",
+  "30°",
+  "a + b + c",
+  "Hypotenuse",
+  "45°",
+];
+
+function calculateScore() {
+  const formResults = new FormData(quizform);
+  let score = 0,
+    index = 0;
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+  }
+  outputMessage.innerText = "The score is " + score;
+}
+
+submitBtn.addEventListener("click", calculateScore);
